2273200a43f51ebd5d8508dbc67396ef1d2a8ac3
ralferlebach <ralf.erlebach@gmx.de>
2024-10-04 13:03:07 +0200

    Update

--- catmodel/pcmgeneralized/classes/pcmgeneralized.php
+++ catmodel/pcmgeneralized/classes/pcmgeneralized.php
@@ -133,7 +133,7 @@ class pcmgeneralized extends model_multiparam {
      * @return array
      */
     public static function get_parameter_names(): array {
-        return ['intercepts', 'discrimination'];
+        return ['intercepts', 'discrimination', 'difficulty'];
 
     }
 
@@ -190,7 +190,7 @@ class pcmgeneralized extends model_multiparam {
     public static function calculate_mean_difficulty(array $ip): float {
         $ip['intercepts'] = self::sanitize_fractions($ip['intercepts']);
         $fractions = self::get_fractions($ip['intercepts']);
-        $kmax = max(array_keys($fractions));
+        $kmax = count($fractions)-1;
 
         return ($ip['intercepts'][$fractions[1]] + $ip['intercepts'][$fractions[$kmax]]) / 2;
     }
@@ -212,7 +212,7 @@ class pcmgeneralized extends model_multiparam {
         $b = $ip['discrimination'];
 
         $fractions = self::get_fractions($a);
-        $kmax = max(array_keys($fractions));
+        $kmax = count($fractions)-1;
 
         // Calculation the denominator of the formulae.
         $denominator = 0;
@@ -260,7 +260,7 @@ class pcmgeneralized extends model_multiparam {
         $b = $ip['discrimination'];
 
         $fractions = self::get_fractions($a);
-        $kmax = max(array_keys($fractions));
+        $kmax = count($fractions)-1;
 
         // Calculation the denominator of the formulae.
         $denominator = 0;
@@ -291,7 +291,7 @@ class pcmgeneralized extends model_multiparam {
         $b = $ip['discrimination'];
 
         $fractions = self::get_fractions($a);
-        $kmax = max(array_keys($fractions));
+        $kmax = count($fractions)-1;
 
         // Calculation the denominator of the formulae.
         $denominator = 0;
