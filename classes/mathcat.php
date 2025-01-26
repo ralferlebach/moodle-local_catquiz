1f926acbebeb836076700e69f2845e95596a8c18
ralferlebach <ralf.erlebach@gmx.de>
2025-01-26 21:15:30 +0100

    Update mathcat.php

--- classes/mathcat.php
+++ classes/mathcat.php
@@ -63,13 +63,13 @@ class mathcat {
         $parameter = $parameterstart;
         $parameterstructure = self::array_to_vector($parameter);
         $steplength = 1;
-        $mxparameter = new matrix($parameter);
+        $mxparameter = new matrix ($parameter);
 
         // Calculate the function values from the given functions for current $parameter.
         $valfunction = $fnfunction(self::vector_to_array($parameter, $parameterstructure));
 
         // Note: Takte the identity matrx as first approximation of the inverse Hessian.
-        $mxinvhessian = (new matrix(count($parameter), count($parameter)))->identity();
+        $mxinvhessian = (new matrix (count($parameter), count($parameter)))->identity();
 
         $valjacobian = $fnderivative(self::vector_to_array($parameter, $parameterstructure));
         $mxgradient = new matrix ($valjacobian); // Note: Line vector.
@@ -83,8 +83,8 @@ class mathcat {
             }
 
             print_r($mxgradient);
-            
-            $mxdirection = $mxinvhessian->multiply(($mxgradient->transpose()));
+
+            $mxdirection = $mxinvhessian->multiply($mxgradient->transpose());
 
             // Note: Perform line search sensitive to given limitations.
             $directionlength = $mxdirection->rooted_summed_squares();
@@ -115,7 +115,7 @@ class mathcat {
             } while ($valfunctionnew < $valfunctiontest);
 
             $valjacobian = $fnderivative(self::vector_to_array(((array) $mxparameternew)[0], $parameterstructure));
-            $mxgradientnew = new ($valjacobian);
+            $mxgradientnew = new matrix ($valjacobian);
 
             $mxparameterdiff = $mxparameternew->subtract($mxparameter);
             $mxgradientdiff = $mxgradientnew->subtract($mxgradient);
