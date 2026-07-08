3c4dfee0d536ae0e094a4ecbd31b55b7d79bec86
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2026-07-08 08:57:32 +0200

    Version (2026070800)

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_catquiz';
-$plugin->supported = [405, 405];
+$plugin->supported = [405, 500];
 $plugin->release = '1.1.4';
-$plugin->version = 2026062400;
+$plugin->version = 2026070800;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
